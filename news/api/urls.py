4301ec33770b876a8c7cd01959138a01dae2f962
Umut Bektaş <info@umutbektas.com>
2019-05-20 17:48:30 +0300

    API View Class

--- news/api/urls.py
+++ news/api/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from .views import (
-article_list_create_api_view,
-article_detail_api_view
+    ArticleListCreateAPIView,
+    ArticleDetailAPIView
 )
 
+# from .views import (
+# article_list_create_api_view,
+# article_detail_api_view
+# )
+
 urlpatterns = [
-    path("articles/", article_list_create_api_view, name="article-list"),
-    path("articles/<int:pk>", article_detail_api_view, name="article-detail")
+    path("articles/", ArticleListCreateAPIView.as_view(), name="article-list"),
+    path("articles/<int:pk>", ArticleDetailAPIView.as_view(), name="article-detail")
+    # path("articles/", article_list_create_api_view, name="article-list"),
+    # path("articles/<int:pk>", article_detail_api_view, name="article-detail")
+
 ]
